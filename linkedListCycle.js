22b8cccc6e5c731d14ab1b1267a73a04897f9b12
mverzi <maggie.verzi@gmail.com>
2023-09-29 17:33:51 -0400

    First commit

---
+++ linkedListCycle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function(head) {
+    var slow = head;
+    var fast = head;
+    
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast)
+            return true;
+    }
+    return false;
+};
